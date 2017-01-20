a36b126de236120f2a67865b7859427425f301e3
leihong.pan <leihong.pan@shuyun.com>
2017-01-20 09:59:58 +0800

    issue:null message:init pro

---
+++ src/main/java/org/harbinuniversity/predictionmodel/PredictionmodelApplication.java
@@ -0,0 +1,12 @@
+package org.harbinuniversity.predictionmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionmodelApplication.class, args);
+	}
+}
